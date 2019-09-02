a6a7e6474e65736524e43d1f9f6eb10042ded130
Nikijs <nikijs299@gmail.com>
2019-09-02 01:05:37 +0100

    Timer added & Class created to hold loading of cards

---
+++ Solitaire/Solitaire/LoadCards.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Solitaire
+{
+    class LoadCards
+    {
+        public void LoadImages(List<CardsObject> card)
+        {
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AH.png"), IsVisible = false }, "AceHearts", false, 14));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AD.png"), IsVisible = false }, "AceDiamonds", false, 14));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KH.png"), IsVisible = false }, "KingHearts", false, 13));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KD.png"), IsVisible = false }, "AceDiamonds", false, 13));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QH.png"), IsVisible = false }, "QueenHearts", false, 12));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QD.png"), IsVisible = false }, "QueenDiamonds", false, 12));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JH.png"), IsVisible = false }, "JackHearts", false, 11));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JD.png"), IsVisible = false }, "JackDiamonds", false, 11));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10H.png"), IsVisible = false }, "TenHearts", false, 10));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10D.png"), IsVisible = false }, "TenDiamonds", false, 10));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9H.png"), IsVisible = false }, "NineHearts", false, 9));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9D.png"), IsVisible = false }, "NineDiamonds", false, 9));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8H.png"), IsVisible = false }, "EightHearts", false, 8));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8D.png"), IsVisible = false }, "EightDiamonds", false, 8));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7H.png"), IsVisible = false }, "SevenHearts", false, 7));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7D.png"), IsVisible = false }, "SevenDiamonds", false, 7));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6H.png"), IsVisible = false }, "SixHearts", false, 6));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6D.png"), IsVisible = false }, "SixDiamonds", false, 6));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5H.png"), IsVisible = false }, "FiveHearts", false, 5));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5D.png"), IsVisible = false }, "FiveDiamonds", false, 5));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4H.png"), IsVisible = false }, "FourHearts", false, 4));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4D.png"), IsVisible = false }, "FourDiamonds", false, 4));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3H.png"), IsVisible = false }, "ThreeHearts", false, 3));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3D.png"), IsVisible = false }, "ThreeDiamonds", false, 3));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2H.png"), IsVisible = false }, "TwoHearts", false, 2));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2D.png"), IsVisible = false }, "TwoDiamonds", false, 2));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AS.png"), IsVisible = false }, "AceSpades", true, 14));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AC.png"), IsVisible = false }, "AceClubs", true, 14));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KS.png"), IsVisible = false }, "KingSpades", true, 13));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KC.png"), IsVisible = false }, "AceClubs", true, 13));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QS.png"), IsVisible = false }, "QueenSpades", true, 12));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QC.png"), IsVisible = false }, "QueenClubs", true, 12));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JS.png"), IsVisible = false }, "JackSpades", true, 11));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JC.png"), IsVisible = false }, "JackClubs", true, 11));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10S.png"), IsVisible = false }, "TenSpades", true, 10));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10C.png"), IsVisible = false }, "TenClubs", true, 10));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9S.png"), IsVisible = false }, "NineSpades", true, 9));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9C.png"), IsVisible = false }, "NineClubs", true, 9));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8S.png"), IsVisible = false }, "EightSpades", true, 8));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8C.png"), IsVisible = false }, "EightClubs", true, 8));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7S.png"), IsVisible = false }, "SevenSpades", true, 7));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7C.png"), IsVisible = false }, "SevenClubs", true, 7));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6S.png"), IsVisible = false }, "SixSpades", true, 6));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6C.png"), IsVisible = false }, "SixClubs", true, 6));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5S.png"), IsVisible = false }, "FiveSpades", true, 5));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5C.png"), IsVisible = false }, "FiveClubs", true, 5));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4S.png"), IsVisible = false }, "FourSpades", true, 4));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4C.png"), IsVisible = false }, "FourClubs", true, 4));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3S.png"), IsVisible = false }, "ThreeSpades", true, 3));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3C.png"), IsVisible = false }, "ThreeClubs", true, 3));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2S.png"), IsVisible = false }, "TwoSpades", true, 2));
+            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2C.png"), IsVisible = false }, "TwoClubs", true, 2));
+
+        }
+    }
+}
--- Solitaire/Solitaire/MainPage.xaml.cs
+++ Solitaire/Solitaire/MainPage.xaml.cs
@@ -12,78 +12,25 @@ namespace Solitaire
     public partial class MainPage : ContentPage
     {
 
-        private CardsObject black;
-        private CardsObject red;
+        public bool game = true;
         Random random = new Random();
         public int tapped=0;
+        int timeEllapsed = 1;
         List<CardsObject> cards = new List<CardsObject>();
         List<CardsObject> cardsRemaning = new List<CardsObject>();
         StackLayout decksp = new StackLayout { Padding = new Thickness(10, 10) };
-
-        public void LoadImages(List<CardsObject> card)
-        {
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AH.png"), IsVisible = false }, "AceHearts" , false,14));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AD.png"), IsVisible = false }, "AceDiamonds", false,14));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KH.png"), IsVisible = false }, "KingHearts", false,13));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KD.png"), IsVisible = false }, "AceDiamonds", false,13));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QH.png"), IsVisible = false }, "QueenHearts", false,12));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QD.png"), IsVisible = false }, "QueenDiamonds", false,12));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JH.png"), IsVisible = false }, "JackHearts", false,11));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JD.png"), IsVisible = false }, "JackDiamonds", false,11));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10H.png"), IsVisible = false }, "TenHearts", false,10));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10D.png"), IsVisible = false }, "TenDiamonds", false,10));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9H.png"), IsVisible = false }, "NineHearts", false,9));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9D.png"), IsVisible = false }, "NineDiamonds", false,9));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8H.png"), IsVisible = false }, "EightHearts", false,8));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8D.png"), IsVisible = false }, "EightDiamonds", false,8));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7H.png"), IsVisible = false }, "SevenHearts", false,7));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7D.png"), IsVisible = false }, "SevenDiamonds", false,7));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6H.png"), IsVisible = false }, "SixHearts", false,6));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6D.png"), IsVisible = false }, "SixDiamonds", false,6));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5H.png"), IsVisible = false }, "FiveHearts", false,5));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5D.png"), IsVisible = false }, "FiveDiamonds", false,5));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4H.png"), IsVisible = false }, "FourHearts", false,4));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4D.png"), IsVisible = false }, "FourDiamonds", false,4));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3H.png"), IsVisible = false }, "ThreeHearts", false,3));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3D.png"), IsVisible = false }, "ThreeDiamonds", false,3));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2H.png"), IsVisible = false }, "TwoHearts", false,2));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2D.png"), IsVisible = false }, "TwoDiamonds", false,2));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AS.png"), IsVisible = false }, "AceSpades", true,14));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.AC.png"), IsVisible = false }, "AceClubs", true,14));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KS.png"), IsVisible = false }, "KingSpades", true,13));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.KC.png"), IsVisible = false }, "AceClubs", true,13));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QS.png"), IsVisible = false }, "QueenSpades", true,12));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.QC.png"), IsVisible = false }, "QueenClubs", true,12));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JS.png"), IsVisible = false }, "JackSpades", true,11));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.JC.png"), IsVisible = false }, "JackClubs", true,11));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10S.png"), IsVisible = false }, "TenSpades", true,10));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.10C.png"), IsVisible = false }, "TenClubs", true,10));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9S.png"), IsVisible = false }, "NineSpades", true,9));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.9C.png"), IsVisible = false }, "NineClubs", true,9));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8S.png"), IsVisible = false }, "EightSpades", true,8));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.8C.png"), IsVisible = false }, "EightClubs", true,8));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7S.png"), IsVisible = false }, "SevenSpades", true,7));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.7C.png"), IsVisible = false }, "SevenClubs", true,7));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6S.png"), IsVisible = false }, "SixSpades", true,6));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.6C.png"), IsVisible = false }, "SixClubs", true,6));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5S.png"), IsVisible = false }, "FiveSpades", true,5));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.5C.png"), IsVisible = false }, "FiveClubs", true,5));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4S.png"), IsVisible = false }, "FourSpades", true,4));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.4C.png"), IsVisible = false }, "FourClubs", true,4));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3S.png"), IsVisible = false }, "ThreeSpades", true,3));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.3C.png"), IsVisible = false }, "ThreeClubs", true,3));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2S.png"), IsVisible = false }, "TwoSpades", true,2));
-            card.Add(new CardsObject(new Image { Source = ImageSource.FromResource("Solitaire.Cards.2C.png"), IsVisible = false }, "TwoClubs", true,2));
-
-        }
+        public int selectedPriority; //priority of card that is selected
+        public Boolean isBlack;
 
             public MainPage()
         {
             InitializeComponent();
-            LoadImages(cards);
+            LoadCards load = new LoadCards();
+            load.LoadImages(cards);
             placeCards();
             placeDeck();
             Shuffle(cards);
+            startTimer();
         }
 
         private void placeDeck()
@@ -118,14 +65,16 @@ namespace Solitaire
                     decksp.VerticalOptions = LayoutOptions.Fill;
 
                     deckGrid.Children.Add(decksp);
+                   
 
-                    
                     Thread.Sleep(50);
                 }
             }
             
             cardsRemaning = cards;
         }
+
+       
 
         public void placeCards()
         {
@@ -162,28 +111,47 @@ namespace Solitaire
 
                     cardsGrid.Children.Add(sp);
 
+                    TapGestureRecognizer tap3 = new TapGestureRecognizer();
+                    tap3.Tapped += Tap_Tapped3;
+                    tap3.NumberOfTapsRequired = 1;
+                    cards[randomNumber].Image.GestureRecognizers.Add(tap3);
+
                     Grid.SetRow(sp, i);
                     Grid.SetColumn(sp, j);
-                    cards.RemoveAt(randomNumber);
+                    cards.RemoveAt(randomNumber); //eliminate duplicates 
                     Thread.Sleep(50);
                 }
             }
             
         }
 
+        private void Tap_Tapped3(object sender, EventArgs e)
+        {
+            Image stackCard = (Image)sender;
+           
+
+
+            //selectedPriority = cards[clicked card].Priority;
+           // isBlack = cards[clicked card].IsBlack;
+        }
+
         private void Tap_Tapped(object sender, EventArgs e)
         {
             tapped++;
-            
+            //make card visible
             cards[tapped].Image.IsVisible = true;
-            //cycle the whole deck begin at index 0
-           
+
+            
             //at 3 taps removes a card from grid number 1
             if (tapped > 3)
             {
                 decksp.Children.RemoveAt(1);
-                
+                //undo the select of card
+                var lastUndo = decksp.Children.ElementAt(2);
+                lastUndo.HeightRequest = 100;
+                lastUndo.BackgroundColor = Color.Default;
             }
+            //remove cards and start stack from the top
             if (tapped == cards.Count - 1)
             {
                 tapped = 1;
@@ -191,8 +159,37 @@ namespace Solitaire
                 decksp.Children.RemoveAt(1);
                 
             }
+            //add image to the stack
             decksp.Children.Add(cards[tapped].Image);
+            //tapped event for card on top 
+            if (tapped > 2)
+            {
+                Image last = (Image)decksp.Children[3];
+                //save selected card properties 
+                selectedPriority = cards[tapped].Priority;
+                isBlack = cards[tapped].IsBlack;
+                
+                labelVar.Text = "Priority: " + selectedPriority + " black? "+ isBlack.ToString();
+                //tap event on card
+                TapGestureRecognizer tap2 = new TapGestureRecognizer();
+                tap2.Tapped += Tap_Tapped2;
+                tap2.NumberOfTapsRequired = 1;
+                last.GestureRecognizers.Add(tap2);
+            }
         }
+
+        //tap event for select card in deck
+        private void Tap_Tapped2(object sender, EventArgs e)
+        {
+            //change 4th element in stack to red
+            var last = decksp.Children.ElementAt(decksp.Children.Count - 1);
+            last.HeightRequest = 110;
+
+            last.BackgroundColor = Color.Red;
+        }
+
+
+
         //Fisher-Yates shuffle acquired from stack overflow 
         private void Shuffle(List<CardsObject> list)
         {
@@ -205,6 +202,25 @@ namespace Solitaire
                 list[k] = list[n];
                 list[n] = value;
             }
+        }
+
+        public void startTimer()
+        {
+            //increments timer every second
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                labelVar2.Text = "Timer: " + timeEllapsed;
+                timeEllapsed++;
+
+                return game;
+            });
+
+
+            labelVar2.TextColor = Color.DarkGreen;
+            labelVar2.FontSize = 15;
+
+            labelVar2.HorizontalTextAlignment = TextAlignment.Start;
+            labelVar2.VerticalTextAlignment = TextAlignment.Start;
         }
 
     }
